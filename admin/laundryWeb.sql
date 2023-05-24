a506f16231d3853b239b512b0a9aa9d46c6d43e9
Maxlaseave <89351211+Maxlaseave@users.noreply.github.com>
2023-05-24 22:18:55 +0800

    asdasd

--- admin/laundryWeb.sql
+++ admin/laundryWeb.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 23, 2023 at 07:05 PM
+-- Generation Time: May 24, 2023 at 04:18 PM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.2.4
 
@@ -35,7 +35,7 @@ CREATE TABLE `dropoff` (
   `bookingDate` date DEFAULT NULL,
   `additionalRemarks` varchar(255) DEFAULT NULL,
   `userId` int(11) DEFAULT NULL,
-  `status` enum('Pending','Confirmed','Completed') NOT NULL
+  `status` enum('Pending','Confirmed','In Progress','Completed','Denied') NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
@@ -43,8 +43,8 @@ CREATE TABLE `dropoff` (
 --
 
 INSERT INTO `dropoff` (`dropOffId`, `numberOfLoads`, `serviceType`, `timeSlot`, `bookingDate`, `additionalRemarks`, `userId`, `status`) VALUES
-('D646ba17a3', 3, '', '09:00:00', '2023-05-24', 'pick up later', 2, 'Pending'),
-('D646bc1ffe', 3, 'Wash and Dry', '10:30:00', '2023-05-25', 'fold too', 1, 'Pending');
+('D234fsd', 3, 'Wash and Dry', '03:00:00', '2023-05-18', 'adasda', 2, 'In Progress'),
+('D646dd7bc3', 1, 'Wash and Dry', '09:27:00', '2023-05-26', 'fold', 2, 'Confirmed');
 
 -- --------------------------------------------------------
 
@@ -60,7 +60,7 @@ CREATE TABLE `selfservice` (
   `bookingDate` date DEFAULT NULL,
   `additionalRemarks` varchar(255) DEFAULT NULL,
   `userId` int(11) DEFAULT NULL,
-  `status` enum('Pending','Confirmed','Completed') NOT NULL
+  `status` enum('Pending','Confirmed','In Progress','Completed','Denied') NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
@@ -69,8 +69,9 @@ CREATE TABLE `selfservice` (
 
 INSERT INTO `selfservice` (`selfServiceId`, `numberOfLoads`, `serviceType`, `timeSlot`, `bookingDate`, `additionalRemarks`, `userId`, `status`) VALUES
 ('S646bc09d5', 2, 'Wash and Dry', '15:20', '2023-05-25', 'yeahh', 1, 'Pending'),
-('S646bc1627', 2, 'Wash Only', '22:44', '2023-05-24', '', 1, 'Pending'),
-('S646bc1ac3', 4, 'Wash and Dry', '09:30', '2023-05-24', 'broommsdad', 1, 'Pending');
+('S646bc1627', 2, 'Wash Only', '22:44', '2023-05-24', '', 1, 'Completed'),
+('S646bc1ac3', 4, 'Wash and Dry', '09:30', '2023-05-24', 'broommsdad', 1, 'Denied'),
+('S646dd7a33', 4, 'Wash and Dry', '11:30', '2023-05-25', 'bring own soap', 2, 'Completed');
 
 -- --------------------------------------------------------
 
@@ -94,8 +95,8 @@ CREATE TABLE `users` (
 INSERT INTO `users` (`userId`, `name`, `email`, `password`, `contactNo`, `role`) VALUES
 (1, 'Nayeon Im', 'nayeon@gmail.com', 'de10825d7a3d9c225a18ba765a55c906', 912345567, 'customer'),
 (2, 'Mina Myoui', 'mina@gmail.com', '308a29557d589f5e2c9d9b0975ff9c82', 991213435, 'customer'),
-(10, 'Kim Dahyun', 'tofu@gmail.com', '37f1a796f6baecf44c808fa1d9c44c21', 912345678, 'Admin'),
-(11, 'Park Jihyo', 'jihyo@gmail.com', '0aa922c50205946138dcd1da47fdba33', 989458645, 'Admin');
+(15, 'Son Chaeyoung', 'chae@gmail.com', 'd4527a3e60fa2458ee74add44bcd9c91', 9345345, 'Employee'),
+(17, 'Kim Dahyunnn', 'tofu@gmail.com', '37f1a796f6baecf44c808fa1d9c44c21', 5834345, 'Admin');
 
 --
 -- Indexes for dumped tables
@@ -129,7 +130,7 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `userId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+  MODIFY `userId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
 
 --
 -- Constraints for dumped tables
